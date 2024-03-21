bd7ba7dfd51806436179eb6e7a744a06b9f8bfdc
Isaiah <ia2099@email.vccs.edu>
2024-03-21 13:47:46 -0400

    I think that SetLambda is finished , but check and correct as needed.

---
+++ cs-lab8/src/SetLambda.java
@@ -0,0 +1,34 @@
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * This class demonstrates the use of lambda expressions and Java streams
+ * to filter and map elements within a Set.
+ * 
+ * @author Joseph Kabesha 
+ * @author Isaiah Ayres
+ * @author Dan Obrien
+ */
+public class SetLambda {
+    public static void main(String[] args) {
+        // Create a HashSet to store integers
+        HashSet<Integer> set2 = new HashSet<>();
+
+        // Populate the set with numbers from 1 to 20
+        for (int i = 1; i <= 20; i++) {
+            set2.add(i);
+        }
+        
+        // Print the initial set
+        System.out.println(set2);
+        
+        // Filter even numbers and then map to square each number
+        Set<Integer> squaredSet = set2.stream()
+                                      .filter(i -> i % 2 == 0)          // Filter even numbers
+                                      .map(i -> i * i)                  // Square each number
+                                      .collect(Collectors.toSet());     // Collect the results into a new set
+        // Print the squared set
+        squaredSet.forEach(System.out::println);
+    }
+}
