ae5d2a69807561097ed331b7c92a1c53b78d38df
Isaiah <ia2099@email.vccs.edu>
2024-03-21 19:25:58 -0400

    I think I'm done with the lab, so I'm committing. You can check out the changes in the files. Also, how is the progress on lab 6?

--- cs-lab8/src/SetIterators.java
+++ cs-lab8/src/SetIterators.java
@@ -1,3 +1,34 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+/**
+ * This class demonstrates the use of an iterator to traverse a Set
+ * and print out only the even elements.
+ * 
+ * @author Joseph Kabesha 
+ * @author Isaiah Ayres
+ * @author Dan Obrien
+ */
 public class SetIterators {
-    
+    public static void main(String[] args) {
+        // Create a HashSet to store integers
+        HashSet<Integer> set = new HashSet<>();
+
+        // Populate the set with numbers from 1 to 20
+        for (int i = 1; i <= 20; i++) {
+            set.add(i);
+        }
+
+        // Create an iterator for the set
+        Iterator<Integer> iterator = set.iterator();
+        
+        // Iterate through the set using the iterator
+        while (iterator.hasNext()) {
+            int num = iterator.next();
+            // Check if the number is even
+            if (num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
+    }
 }
--- cs-lab8/src/SetOperations.java
+++ cs-lab8/src/SetOperations.java
@@ -1,3 +1,49 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * This class demonstrates the use of Sets to perform Union, Intersection, and Difference operations.
+ * 
+ * The class populates two sets with integers and then performs union, intersection, and difference operations on them.
+ * It prints out the original sets and the results of the operations.
+ * 
+ * @author Joseph Kabesha 
+ * @author Isaiah Ayres
+ * @author Dan Obrien
+ */
 public class SetOperations {
-    
+    public static void main(String[] args) {
+        // Create two sets
+        HashSet<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+
+        // Create a second set
+        HashSet<Integer> set2 = new HashSet<>();
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+
+        // Print the original sets
+        System.out.println("Set 1: " + set1);
+        System.out.println("Set 2: " + set2);
+
+        // Union: Combine elements from both sets, removing duplicates
+        Set<Integer> union = new HashSet<>(set1);
+        union.addAll(set2);
+        System.out.println("Union: " + union);
+
+        // Intersection: Find common elements present in both sets
+        Set<Integer> intersection = new HashSet<>(set1);
+        intersection.retainAll(set2);
+        System.out.println("Intersection: " + intersection);
+
+        // Difference (elements in Set1 but not in Set2): Find elements present in Set1 but not in Set2
+        Set<Integer> difference = new HashSet<>(set1);
+        difference.removeAll(set2);
+        System.out.println("Difference (Set1 - Set2): " + difference);
+    }
 }
